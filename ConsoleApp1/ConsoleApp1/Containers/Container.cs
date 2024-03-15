956bc4e6daf88d35dcb9bcfbf528ac2e339bed51
s27467 <s27467@pjwstk.edu.pl>
2024-03-15 14:41:36 +0100

    Kod z lekcji v1

---
+++ ConsoleApp1/ConsoleApp1/Containers/Container.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleApp1.Interfaces;
+
+namespace ConsoleApp1;
+
+public abstract class Container : IContainer
+{
+    public double CargoWeight { get; set; }
+    protected Container(double cargoWeight)
+    {
+        CargoWeight = cargoWeight;
+    }
+
+
+    public virtual void Unload()
+    {
+        if (CargoWeight > 100)
+        {
+            throw new OverflowException();
+        }
+        throw new NotImplementedException();
+    }
+
+    public virtual void Load(double cargoWeight)
+    {
+        throw new NotImplementedException();
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using ConsoleApp1;
+
 Console.WriteLine("Hello, World!");
+
+var list = new List<int>() {1,2,3};
+Dictionary<string, int> dict = new Dictionary<string, int>();
+Dictionary<PossibleProducts, double> products;
